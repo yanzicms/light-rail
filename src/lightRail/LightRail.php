b8f22aa29f35a0aaece1cac6253828f25285bac5
xwlrbh <xwlrbh@sina.cn>
2022-06-07 14:44:18 +0800

    Upgrade

--- src/lightRail/LightRail.php
+++ src/lightRail/LightRail.php
@@ -13,7 +13,7 @@ class LightRail
     public function run()
     {
         define('IS_CLI', PHP_SAPI == 'cli' ? true : false);
-        define('VERSION', '1.1.0');
+        define('VERSION', '1.2.0');
         Debug::remark('begin', 'both');
         Config::load();
         date_default_timezone_set(Config::get('timezone'));
